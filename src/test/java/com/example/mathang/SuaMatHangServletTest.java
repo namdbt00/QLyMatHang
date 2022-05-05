da14212abf03c1872b86a40f974e5508f58e150d
NgocToan-dev <62018760+NgocToan-dev@users.noreply.github.com>
2022-05-05 21:26:55 +0700

    Thêm thư mục test cho servlet

---
+++ src/test/java/com/example/mathang/SuaMatHangServletTest.java
@@ -0,0 +1,44 @@
+package com.example.mathang;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SuaMatHangServletTest {
+
+    @Test
+    void doGet() {
+    }
+
+    @Test
+    void doPost() {
+    }
+
+    @Test
+    void createListAtbFromJsonString() {
+    }
+
+    @Test
+    void createListUnitFromJsonString() {
+    }
+
+    @Test
+    void updateMatHang() {
+    }
+
+    @Test
+    void getCategoryById() {
+    }
+
+    @Test
+    void buildMatHang() {
+    }
+
+    @Test
+    void buildMatHangWithoutImage() {
+    }
+
+    @Test
+    void toUTF8String() {
+    }
+}
